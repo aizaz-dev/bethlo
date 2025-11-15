ab16429188e76d7e2787df31dcc608024035d3e2
iAsadDev <youremail@domain.com>
2025-11-15 21:42:04 +0500

    update gallery page

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,17 +1,37 @@
 "use client";
 
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function ComingSoon() {
   return (
     <main className="max-w-7xl mx-auto px-6 sm:px-8 py-12 pt-55 text-gray-700 leading-relaxed">
-      <Image
-        src="/Gallery/gallery.jpg"
-        width={200}
-        height={300}
-        alt="Coming Soon"
-        className="w-full rounded-2xl shadow-lg object-cover"
-      />
+
+      <a
+        href="https://your-external-link.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <motion.div
+          initial={{ y: "-100vh", opacity: 0 }}  
+          animate={{ y: 0, opacity: 1 }}       
+          transition={{
+            type: "spring",
+            stiffness: 110,
+            damping: 12,
+            mass: 0.7,
+          }}
+        >
+          <Image
+            src="/Gallery/gallery.jpg"
+            width={200}
+            height={300}
+            alt="Coming Soon"
+            className="w-full rounded-2xl shadow-lg object-cover cursor-pointer"
+          />
+        </motion.div>
+      </a>
+
     </main>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8 py-8 md:py-12 pt-35 flex flex-col pt-16 md:pt-55 items-center justify-center text-center text-text-primary">
+    <main className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8 py-8 md:py-12 pt-35npm flex flex-col pt-16 md:pt-55 items-center justify-center text-center text-text-primary">
       
       {/* Hero Message */}
       <h1 className="font-roboto font-light text-2xl sm:text-3xl md:text-[35px] leading-7 sm:leading-8 md:leading-[36px] text-[#707070] mb-4 sm:mb-5 py-2 sm:py-3 px-4">
